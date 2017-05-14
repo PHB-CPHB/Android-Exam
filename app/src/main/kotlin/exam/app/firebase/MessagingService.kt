f0c80cad6c66244da98b52dffe1cbb80d19c012d
Mikkel Ziemer <mzhjensen@gmail.com>
2017-05-14 17:44:47 +0200

    Added firebase support properly

---
+++ app/src/main/kotlin/exam/app/firebase/MessagingService.kt
@@ -0,0 +1,10 @@
+package exam.app.firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Created by Mikkel on 14/05/2017.
+ */
+class MessagingService : FirebaseMessagingService(){
+
+}
